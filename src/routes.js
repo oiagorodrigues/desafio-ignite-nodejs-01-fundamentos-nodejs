380f0044e592954e763cc47092c659057e149bd5
Iago Rodrigues <yagomello16@gmail.com>
2023-03-24 10:49:58 -0300

    feat: add put route to update a resource

--- src/routes.js
+++ src/routes.js
@@ -45,5 +45,28 @@ export const routes = [
 
       return res.end(JSON.stringify(tasks))
     }
+  },
+  {
+    method: 'PUT',
+    path: buildRoutePath('/tasks/:id'),
+    handler(req, res) {
+      const { id } = req.params
+      const { title, description } = req.body
+
+      const task = db.find('tasks', id)
+
+      if (!task) {
+        return res.writeHead(404).end(`Task not found.`)
+      }
+
+      const updatedTask = db.update('tasks', id, {
+        ...task,
+        title: title ?? task.title,
+        description: description ?? task.description,
+        updated_at: new Date().toISOString()
+      })
+
+      return res.end(JSON.stringify(updatedTask))
+    }
   }
 ]
